5aa3a2176765de285ec64dd16ef6986e48c640cf
Thomas Espervik <thomas.espervik@me.com>
2019-11-25 09:09:19 +0100

    Added several keywords for actions

--- main.py
+++ main.py
@@ -37,6 +37,13 @@ def tw(text, sleep=.05):
   print('')
   time.sleep(1)
 
+#input tables
+straight = ['straight', 'ahead', 'forward', 'up']
+back = ['back', 'return', 'down', 'run', 'run away']
+interact = ['take', 'steal', 'yoink', 'use', 'pick', 'pick up', 'interact', 'investigate', 'search' 'yeet', 'push']
+kill = ['kill', 'slaugher', 'murder', 'stab', 'decapitate']
+
+
 print("WARNING: Please do not enter anything into the terminal while text is being displayed. \nIt may interfere with choice making functions. \nI am way too lazy to do anything about that.")
 time.sleep(5)
 clearScreen()
@@ -69,16 +76,16 @@ while True:
   print("What do you want to do? \n - Enter the mansion \n - Go around the mansion")
   choice = input("> ")
   clearScreen()
-  if choice.lower() == "enter the mansion":
+  if "enter" in choice.lower():
     tw("As you approach the entrance, the doors open by themselves, almost as if they were magical. You step through the doors, and suddenly they close shut. You try opening the doors again, but they are firmly stuck in place. You have been trapped in the mansion.")
     while True:
       clearScreen()
       hallwayStraight()
       tw("You find yourself in a hallway right past the entrance.")
       newParagraph()
-      print("What do you want to do? \n - Walk down the hallway \n - Turn around")
+      print("What do you want to do? \n - Walk straight ahead \n - Turn around")
       choice = input("> ")
-      if choice.lower() == "walk down the hallway":
+      if choice.lower() in straight:
         getKey = 0
         while True:
           clearScreen()
@@ -89,7 +96,7 @@ while True:
           choice = input("> ")
           if choice.lower() == "take fork":
             tw("HAHAHAHAHA aren't you funny")
-          elif choice.lower() == "straight ahead":
+          elif choice.lower() in straight:
             while True:
               clearScreen()
               lockedDoor()
@@ -100,11 +107,11 @@ while True:
               elif getKey == 1:
                 print("What do you want to do? \n - Turn around \n - Use simple key")
               choice = input("> ")
-              if choice.lower() == "turn around":
+              if choice.lower() in back:
                 break
-              elif choice.lower() == "use simple key" and getKey == 0:
+              elif choice.lower() in interact and getKey == 0:
                 tw("You do not have the key to open this door.")
-              elif choice.lower() == "use simple key" and getKey == 1:
+              elif choice.lower() in interact and getKey == 1:
                 tw("You open the door using the simple key you found, and head on through.")
                 getEquip = 0
                 getKey = 0
@@ -115,10 +122,10 @@ while True:
                   newParagraph()
                   print("What do you want to do? \n - Turn left \n - Walk straight down \n - Turn right \n - Go back")
                   choice = input("> ")
-                  if choice.lower() == "go back":
+                  if choice.lower() in back:
                     print()
                     tw("There is literally no point in going back to where you came from.")
-                  elif choice.lower() == "turn left":
+                  elif "left" in choice.lower():
                     clearScreen()
                     lockedDoor()
                     tw("It's another locked door...")
@@ -126,14 +133,14 @@ while True:
                     if getKey == 0:
                       print("What do you want to do? \n - Go back")
                       choice = input("> ")
-                      if choice.lower() == "go back":
+                      if choice.lower() in back:
                         continue
                     elif getKey == 1:
                       print("What do you want to do? \n - Go back \n \n - Use rotten key")
                       choice = input("> ")
-                      if choice.lower() == "go back":
+                      if choice.lower() in back:
                         continue
-                      elif choice.lower() == "use rotten key":
+                      elif choice.lower() in interact:
                         tw("You insert the rotten key. The door is rusted and won't unlock so easily, but you manage it, breaking the key in the process. You open the door, climb a flight of stairs, and begin exploring the second floor in your search for freedom.")
                         destroyedWall = 0
                         while True:
@@ -149,11 +156,11 @@ while True:
                             tw("The rock broke through the wall on the left, revealing another path.")
                             print("WHat should you do? \n - Walk to the right \n - Walk to the left \n - Go back")
                           choice = input("> ")
-                          if choice.lower() == "go back":
+                          if choice.lower() in back:
                             print()
                             tw("You have already checked everything downstairs. There is no point in going back now.")
                             time.sleep(1)
-                          elif choice.lower() == "walk to the right":
+                          elif "right" in choice.lower():
                             while True:
                               clearScreen()
                               tRight()
@@ -163,9 +170,9 @@ while True:
                               choice = input("> ")
                               if choice.lower() == "take fork":
                                 tw("Are you kidding me")
-                              elif choice.lower() == "go back":
+                              elif choice.lower() in back:
                                 break
-                              elif choice.lower() == "straight":
+                              elif choice.lower() in straight:
                                 clearScreen()
                                 emptyStaircase()
                                 if destroyedWall == 0:
@@ -174,7 +181,7 @@ while True:
                                   tw("There's a staircase in front of you, but there is seemingly nothing at the top. There is a large hole in the roof, it could be where that giant rock came from. You decide there's no point in climbing the stairs.")
                                 time.sleep(1)
                                 continue
-                              elif choice.lower() == "right":
+                              elif "right" in choice.lower():
                                 while True:
                                   clearScreen()
                                   emptyRoom()
@@ -185,9 +192,9 @@ while True:
                                   elif destroyedWall == 1:
                                     print("What do you want to do? \n - Go back")
                                   choice = input("> ")
-                                  if choice.lower() == "go back":
+                                  if choice.lower() in back:
                                     break
-                                  elif choice.lower() == "search the room further" and destroyedWall == 0:
+                                  elif choice.lower() in interact and destroyedWall == 0:
                                     while True:
                                       print()
                                       tw("You find it suspicious that the room is so barren, so you decide to search it further.")
@@ -196,13 +203,13 @@ while True:
                                       newParagraph()
                                       print("What do you want to do with the brick? \n - Push it back in \n - Nothing")
                                       choice = input("> ")
-                                      if choice.lower() == "nothing":
+                                      if "nothing" in choice.lower():
                                         print()
                                         tw("You decide to leave the loose brick alone")
                                         time.sleep(1)
                                         break
                                       
-                                      elif choice.lower() == "push it back in":
+                                      elif choice.lower() in interact:
                                         print()
                                         tw("You push the brick back in place. Suddenly you hear a distant rumbling. You look behind you and see a huge rock rolling down the hallway you were just in.")
                                         time.sleep(1)
@@ -219,15 +226,15 @@ while True:
                                         time.sleep(1)
                                         break
 
-                                  elif choice.lower() == "search the room further" and destroyedWall == 1:
+                                  elif choice.lower() in interact and destroyedWall == 1:
                                     print()
                                     tw("You already searched the room. There is nothing else to find.")
 
-                          elif choice.lower() == "walk to the left" and destroyedWall == 0:
+                          elif "left" in choice.lower() and destroyedWall == 0:
                             print()
                             tw("That's a wall.")
                           
-                          elif choice.lower() == "walk to the left" and destroyedWall == 1:
+                          elif "left" in choice.lower() and destroyedWall == 1:
                             ##########
                             while True:
                               clearScreen()
@@ -240,17 +247,17 @@ while True:
                     elif getKey == 1:
                       print("What do you want to do? \n - Go back")
                       choice = input("> ")
-                      if choice.lower() == "go back":
+                      if choice.lower() in back:
                         continue
 
-                  elif choice.lower() == "walk straight down":
+                  elif choice.lower() in straight:
                     clearScreen()
                     hallwayStraight()
                     tw("You walk down the hallway. It seems a bit longer than the others...")
                     newParagraph()
                     print("Do you keep walking or go back? \n - Keep walking \n - Go back")
                     choice = input("> ")
-                    if choice.lower() == "keep walking":
+                    if "walk" in choice.lower():
                       clearScreen()
                       hallwayStraight()
                       tw("You keep walking down the hallway, which seems painfully long. You feel like you've been walking forever, but there just is no end to this corridor.")
@@ -271,10 +278,10 @@ while True:
                             newParagraph()
                             print("What do you want to do now? \n - Pick up weapon and armor \n - Go back")
                             choice = input("> ")
-                            if choice.lower() == "pick up weapon and armor":
+                            if choice.lower() in interact:
                               tw("You grab the sword and the worn leather chestplate. Any form of protection might help at this point.")
                               getEquip = 1
-                            elif choice.lower() == "go back":
+                            elif choice.lower() in back:
                               print("Are you really sure you want to go back empty handed after all this time? \n - No")
                               choice = input("> ")
                               if choice.lower() == "no":
@@ -284,14 +291,14 @@ while True:
                             newParagraph()
                             print("What should you do from here? \n - Go back")
                             choice = input("> ")
-                            if choice.lower() == "go back":
+                            if choice.lower() in back:
                               break
                       elif choice.lower() == "no":
                         tw("You decide it's not worth the effort to walk any further, and you begin walking back to where you started.")
                         continue
-                    elif choice.lower() == "go back":
+                    elif choice.lower() in back:
                       continue
-                  elif choice.lower() == "turn right":
+                  elif "right" in choice.lower():
                     while True:
                       clearScreen()
                       skeleton()
@@ -300,10 +307,10 @@ while True:
                       if getEquip == 1:
                         print("Think fast! What should you do? \n - Run away \n \n - Kill skeleton")
                         choice = input("> ")
-                        if choice.lower() == "run away":
+                        if choice.lower() in back: ###############
                           tw("You hurry back out of the room and shut the door behind you. The skeleton shouldn't be able to open it.")
                           break
-                        elif choice.lower() == "kill skeleton":
+                        elif choice.lower() in kill:
                           clearScreen()
                           time.sleep(1)
                           skelHeadPreHit()
@@ -318,31 +325,32 @@ while True:
                       elif getEquip == 0:
                         print("Think fast! What should you do? \n - Run away")
                         choice = input("> ")
-                        if choice.lower() == "run away":
+                        if choice.lower() in back:
                           tw("You hurry back out of the room and shut the door behind you. The skeleton shouldn't be able to open it.")
                           break
-                        elif choice.lower() == "kill skeleton":
+                        elif choice.lower() in kill:
                           tw("You don't have any means to kill it.")
-          elif choice.lower() == "left":
+          elif "left" in choice.lower():
             while True:
               clearScreen()
               emptyRoom()
               if getKey == 0:
                 tw("You enter an empty bedroom. On the night stand lies a simple key.")
               elif getKey == 1:
-                tw("You grab the simple key.")
+                tw("The bedroom is empty")
               newParagraph()
               if getKey == 0:
                 print("What do you do? \n - Grab simple key \n - Go back")
               elif getKey == 1:
                 print("What do you do? \n - Go back")
               choice = input("> ")
-              if choice.lower() == "grab simple key":
+              if choice.lower() in interact:
+                tw("You grab the simple key.")
                 getKey = 1
-              elif choice.lower() == "go back":
+              elif choice.lower() in back:
                 break
 
-      elif choice.lower() == "turn around":
+      elif choice.lower() in back:
         while True:
           clearScreen()
           deadEnd()
@@ -350,10 +358,10 @@ while True:
           newParagraph()
           print("What do you want to do? \n - Turn around")
           choice = input("> ")
-          if choice.lower() == "turn around":
+          if choice.lower() in back:
             break
 
-  elif choice.lower() == "go around the mansion":
+  elif "around" in choice.lower():
     tw("You take a walk around the mansion to check it out. You notice there are no other doors than the main entrance, and all the windows are tightly sealed, making you unable to open any of them.")
     newParagraph()
     tw("You see no other possible option than to return to the main entrance and head inside.")
